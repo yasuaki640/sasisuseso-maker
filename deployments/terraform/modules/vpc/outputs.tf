b9421cbc82c14450e6a4911aa7e1e83009794d6f
yasuaki640 <yasuaki640@gmail.com>
2025-04-13 22:41:52 +0900

    Add buildspec.yml for Docker image build and update ECS configuration

--- deployments/terraform/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "vpc_id" {
-#   description = "The ID of the VPC"
-#   value       = aws_vpc.main.id
-# }
-#
-# output "private_subnet_ids" {
-#   description = "List of private subnet IDs"
-#   value       = aws_subnet.private[*].id
-# }
