af4a11323cf5b441e31e608fbb772bb8ce226862
yasuaki640 <yasuaki640@gmail.com>
2025-04-13 23:18:53 +0900

    Refactor deploy.yml to comment out ECS deployment steps and improve readability

--- deployments/terraform/modules/ecs/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "cluster_id" {
-#   description = "The ID of the ECS cluster"
-#   value       = aws_ecs_cluster.main.id
-# }
-#
-# output "service_name" {
-#   description = "The name of the ECS service"
-#   value       = aws_ecs_service.main.name
-# }
